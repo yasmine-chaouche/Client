adb21b98bcd2f92af180712c6d693eaa505f16bd
Chaouche <hy_chaouche@esi.dz>
2019-12-30 10:02:17 +0100

    second_commit

---
+++ pages/Promotions.vue
@@ -0,0 +1,106 @@
+<template>
+<v-layout>
+    <v-flex class="text-center">
+      <div class="text-center">
+      <img
+        src="/vv.png"
+        alt="Vuetify.js"
+        class="mb-4 "
+        height="120"
+      >
+      </div>
+     <v-container fluid >
+       <v-layout align-center justify-center >
+         <v-flex xs12 sm8 md4 >
+           <v-card class="elevation-20" to="1cp">
+             <v-toolbar dark color="indigo darken-3">
+               <v-toolbar-title>
+                 1CP
+               </v-toolbar-title>
+               <v-spacer></v-spacer>
+               <v-toolbar-title>
+                 2019/2020
+               </v-toolbar-title>
+             </v-toolbar>
+             </v-card>
+             </v-flex>
+             </v-layout>
+    </v-container>
+         <v-container fluid>
+       <v-layout align-center justify-center>
+         <v-flex xs12 sm8 md4>
+           <v-card class="elevation-20" to="2cp">
+             <v-toolbar dark color="indigo darken-3">
+               <v-toolbar-title>
+                 2CP
+               </v-toolbar-title>
+               <v-spacer></v-spacer>
+               <v-toolbar-title>
+                 2019/2020
+               </v-toolbar-title>
+             </v-toolbar>
+             </v-card>
+             </v-flex>
+             </v-layout>
+    </v-container>
+         <v-container fluid>
+       <v-layout align-center justify-center>
+         <v-flex xs12 sm8 md4>
+           <v-card class="elevation-20" to="1cs">
+             <v-toolbar dark color="indigo darken-3">
+               <v-toolbar-title>
+                 1CS
+               </v-toolbar-title>
+               <v-spacer></v-spacer>
+               <v-toolbar-title>
+                 2019/2020
+               </v-toolbar-title>
+             </v-toolbar>
+             </v-card>
+             </v-flex>
+             </v-layout>
+    </v-container>
+         <v-container fluid>
+       <v-layout align-center justify-center>
+         <v-flex xs12 sm8 md4>
+           <v-card class="elevation-20" to="2cs">
+             <v-toolbar dark color="indigo darken-3">
+               <v-toolbar-title>
+                 2CS
+               </v-toolbar-title>
+               <v-spacer></v-spacer>
+               <v-toolbar-title>
+                 2019/2020
+               </v-toolbar-title>
+             </v-toolbar>
+             </v-card>
+             </v-flex>
+             </v-layout>
+    </v-container>
+         <v-container fluid>
+       <v-layout align-center justify-center>
+         <v-flex xs12 sm8 md4>
+           <v-card class="elevation-20" to="3cs">
+             <v-toolbar dark color="indigo darken-3">
+               <v-toolbar-title>
+                 3CS
+               </v-toolbar-title>
+               <v-spacer></v-spacer>
+               <v-toolbar-title>
+                 2019/2020
+               </v-toolbar-title>
+             </v-toolbar>
+             </v-card>
+             </v-flex>
+             </v-layout>
+    </v-container>
+  </v-flex>
+</v-layout>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
